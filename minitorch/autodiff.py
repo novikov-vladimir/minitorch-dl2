d52b2e3cb84601d597b7cdb8a73ac3b8c82868f0
Vladimir Novikov <vladimir-novikov-2004@yandex.ru>
2026-09-14 21:24:39 +0300

    central difference

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,13 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    addit = list(vals)
+    sub = list(vals)
+
+    addit[arg] += epsilon
+    sub[arg] -= epsilon
+
+    return (f(*addit) - f(*sub)) / (2.0 * epsilon)
 
 
 variable_count = 1
